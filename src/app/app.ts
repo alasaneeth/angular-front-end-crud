27698c19eb4c575825e1176485c3b1a08e17e66e
alasaneeth <alasaneeth22@gmail.com>
2026-02-24 21:53:31 +0400

    Angular version Update

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('angular-crud');
+  protected readonly title = signal('my-app');
 }
